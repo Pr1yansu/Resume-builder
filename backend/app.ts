aa3cf2044327cee5fb340d87adbe68a6997c108c
Pr1yansu <Priyansuchowdhury57@gmail.com>
2024-08-03 20:34:56 +0530

    backend changes

--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
+});
+
+process.on("SIGINT", () => {
+  console.log("Server is shutting down");
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  console.log("Server is shutting down");
+  process.exit(0);
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("There was an uncaught error Shutting down the server", err);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(
+    "There was an unhandled rejection Shutting down the server",
+    err
+  );
+  process.exit(1);
+});
